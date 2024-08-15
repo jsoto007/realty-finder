9d38a8bc382cab86976981cb4a2371794f3ab4ec
Jose Soto <jsoto7087@gmail.com>
2024-08-14 20:16:32 -0400

    adds additional Data to CSV file

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import pprint
 import csv
 import datetime
 
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+
+
 
 print("####****Please Enter the Zip-Code****####")
 print("############********############")
@@ -32,14 +37,23 @@ if response.status_code == 200:
   data = response.json()
 
 
-  pp = pprint.PrettyPrinter(indent=4)
     
+
+  pp = pprint.PrettyPrinter(indent=4)
+
   pprint.pp(data)
 
 
-fields = ["objectid",	"assessment_date",	"basements",	"beginning_point",	"book_and_page",	"building_code",	"building_code_description",	"category_code",	"category_code_description",	"census_tract",	"central_air",	"cross_reference",	"date_exterior_condition", "depth",	"exempt_building",	"exempt_land",	"exterior_condition",	"fireplaces",	"frontage",	"fuel",	"garage_spaces",	"garage_type",	"general_construction",	"geographic_ward",	"homestead_exemption",	"house_extension",	"house_number",	"interior_condition",	"location",	"mailing_address_1",	"mailing_address_2",	"mailing_care_of",	"mailing_city_state",	"mailing_street",	"mailing_zip"	"market_value",	"market_value_date",	"number_of_bathrooms",	"number_of_bedrooms",	"number_of_rooms",	"number_stories",	"off_street_open",	"other_building",	"owner_1",	"owner_2",	"parcel_number",	"parcel_shape",	"quality_grade",	"recording_date",	"registry_number",	"sale_date",	"sale_price",	"separate_utilities",	"sewer",	"site_type",	"state_code",	"street_code",	"street_designation",	"street_direction",	"street_name",	"suffix",	"taxable_building",	"taxable_land",	"topography",	"total_area",	"total_livable_area",	"type_heater",	"unfinished",	"unit",	"utility",	"view_type",	"year_built"	"year_built_estimate",	"zip_code",	"zoning",	"pin",	"building_code_new"	"building_code_description_new",	"shape", "time", "rows", "fields", "total_rows", 'building_code_new', 'the_geom', 'mailing_zip', 'market_value', 'the_geom_webmercator', 'year_built', 'cartodb_id', 'year_built_estimate', 'building_code_description_new']
+driver.get("https://www.phila.gov/revenue/realestatetax/")
 
-# file_name = "realty records.csv"
+for row in data['rows']:
+    row["Taxed_Balance"] = "testing test"
+
+
+
+
+
+fields = ["objectid",	"assessment_date",	"basements",	"beginning_point",	"book_and_page",	"building_code",	"building_code_description",	"category_code",	"category_code_description",	"census_tract",	"central_air",	"cross_reference",	"date_exterior_condition", "depth",	"exempt_building",	"exempt_land",	"exterior_condition",	"fireplaces",	"frontage",	"fuel",	"garage_spaces",	"garage_type",	"general_construction",	"geographic_ward",	"homestead_exemption",	"house_extension",	"house_number",	"interior_condition",	"location",	"mailing_address_1",	"mailing_address_2",	"mailing_care_of",	"mailing_city_state",	"mailing_street",	"mailing_zip"	"market_value",	"market_value_date",	"number_of_bathrooms",	"number_of_bedrooms",	"number_of_rooms",	"number_stories",	"off_street_open",	"other_building",	"owner_1",	"owner_2",	"parcel_number",	"parcel_shape",	"quality_grade",	"recording_date",	"registry_number",	"sale_date",	"sale_price",	"separate_utilities",	"sewer",	"site_type",	"state_code",	"street_code",	"street_designation",	"street_direction",	"street_name",	"suffix",	"taxable_building",	"taxable_land",	"topography",	"total_area",	"total_livable_area",	"type_heater",	"unfinished",	"unit",	"utility",	"view_type",	"year_built"	"year_built_estimate",	"zip_code",	"zoning",	"pin",	"building_code_new"	"building_code_description_new",	"shape", "time", "rows", "fields", "total_rows", 'building_code_new', 'the_geom', 'mailing_zip', 'market_value', 'the_geom_webmercator', 'year_built', 'cartodb_id', 'year_built_estimate', 'building_code_description_new', 'Taxed_Balance']
 
 
 rows = data['rows']
@@ -64,22 +78,10 @@ print(f"Data successfully written to {csv_file}")
 
 print("888888888888888888888888888888888888888")
 
-for row in data['rows']:
-    print(row['mailing_street'])
+# for row in data['rows']:
+#     print(row['mailing_street'])
 
-    
 
-print("888888888888888888888888888888888888888")
-print("888888888888888888888888888888888888888")
-print("888888888888888888888888888888888888888")
-
-# Add the "test": "testing test" key to each object
-for row in data['rows']:
-    row["test"] = "testing test"
-
-# Print the modified data
-for row in data['rows']:
-    pprint.pp(row)
 
 
 
